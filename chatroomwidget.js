bd392e2e6a68354bef3102e5535cb737bdeff696
Hraban Luyat <hraban@0brg.net>
2014-04-17 01:50:34 +0200

    more codes

--- chatroomwidget.js
+++ chatroomwidget.js
@@ -4,13 +4,20 @@ var debug;
 
 // appended odd string to avoid shadowing existing vars
 function chatroomwidget_88ed71a($) {
+    function isScrolledToBottom(el) {
+        return el.scrollHeight === (el.offsetHeight + el.scrollTop);
+    }
+    function scrollToBottom(el) {
+        el.scrollTop = el.scrollHeight;
+    }
+
     var id = "chatroomwidget_88ed71a";
     if ($ === undefined) {
         throw "chatroomwidget.js requires jQuery";
     }
     var ws;
-    var $input = $("<form>")
-        .append($("<input id="+id+"input>").click(function (e) {
+    var $input = $("<form style='flex-shrink: 0'>")
+        .append($("<input id="+id+"input style='width: 100%'>").click(function (e) {
                 // don't hide
                 return false;
             })
@@ -26,23 +33,32 @@ function chatroomwidget_88ed71a($) {
             .css({
                 width: "100px",
                 height: "100px",
+                "font-size": '6pt',
             })
             .find('> :not(#'+id+'messages)')
                 .hide();
+        $('#'+id+'messages').css('overflow', 'hidden');
     }
     function toMaxi(node) {
         $(node)
             .removeClass("mini")
             .css({
                 width: "250px",
-                height: "500px",
+                height: "200px",
+                "font-size": '12pt',
             })
             .find('> :not(#'+id+'messages)')
                 .show();
+        scrollToBottom($('#'+id+'messages').css({
+            "overflow-y": "auto",
+            "overflow-x": "hidden",
+        })[0]);
     }
     var $node = $('<div id=' + id + '>')
-        .append("<div id="+id+"messages style='white-space: pre-wrap'>")
-        .append($input)
+        .append($("<div id="+id+"messages>").css({
+                'white-space': 'pre-wrap',
+            })
+        ).append($input)
         .css({
             position: "absolute",
             right: "50px",
@@ -50,6 +66,9 @@ function chatroomwidget_88ed71a($) {
             border: "solid thin black",
             "background-color": "white",
             "z-index": 1000,
+            display: "flex",
+            "flex-direction": "column",
+            "justify-content": "flex-end",
         }).click(function(e) {
             e.preventDefault();
             if ($(this).hasClass("mini")) {
@@ -63,7 +82,12 @@ function chatroomwidget_88ed71a($) {
     ws.onopen = function onopen() {
     };
     ws.onmessage = function onmessage(evt) {
-        $('<div>').text(evt.data).appendTo('#'+id+'messages');
+        var el = document.getElementById(id+'messages');
+        var atbottom = isScrolledToBottom(el);
+        $('<div>').text(evt.data).appendTo(el);
+        if (atbottom || $('#'+id).hasClass("mini")) {
+            scrollToBottom(el);
+        }
     };
     debug = {ws: ws};
 }
