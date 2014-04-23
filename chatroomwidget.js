1071599ccc53744651ce531fb4595c77e246dbdf
Koen Mostert <reaxis@gmail.com>
2014-04-24 01:41:38 +0200

    removed jQuery as a dependency

--- chatroomwidget.js
+++ chatroomwidget.js
@@ -22,10 +22,19 @@
 
 (function () {
 
+    function $(id, parent) {
+        return (parent || document).querySelector(id);
+    }
+
+    function $all(selector, func, parent) {
+        Array.prototype.forEach.call((parent || document).querySelectorAll(selector), func);
+    }
+
     function getWebsocketUrl() {
         if (typeof chatroomwidgetServerUrl !== "undefined") {
             return chatroomwidgetServerUrl;
         }
+
         // Include protocol to prevent leaking between HTTP and HTTPS.
         // Exclude querystring & anchor to normalize for sessions &c
         var myloc = window.location.origin + window.location.pathname;
@@ -33,101 +42,144 @@
     }
 
     // build the chat widget
-    function chatroomwidget_main($) {
+    function chatroomwidget_main() {
         function isScrolledToBottom(el) {
             return el.scrollHeight === (el.offsetHeight + el.scrollTop);
         }
+
         function scrollToBottom(el) {
             el.scrollTop = el.scrollHeight;
         }
-        var name;
-        var id = "chatroomwidget_88ed71a";
-        var ws;
-        var $input = $("<form style='flex-shrink: 0'>")
-            .append($("<input id="+id+"input style='width: 100%'>"))
-            .submit(function (e) {
-                e.preventDefault();
-                var node = document.getElementById(id+"input");
-                ws.send("<" + name + "> " + node.value);
-                node.value = "";
+
+        var name,
+            id = "chatroomwidget_88ed71a",
+            ws;
+
+        var $node = document.createElement("div");
+        $node.id = id;
+        $node.style.position = "absolute";
+        $node.style.right = "50px";
+        $node.style.top = "50px";
+        $node.style.border = "solid thin black";
+        $node.style.backgroundColor = "white";
+        $node.style.zIndex = 1000;
+        $node.style.display = "flex";
+        $node.style.flexDirection = "column";
+        $node.style.justifyContent = "flex-end";
+        $node.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            if ($node.className === "mini") {
+                toMaxi();
+            } else {
+                toMini();
+            }
+        });
+
+        var banner = document.createElement("div");
+        banner.id = id + "banner";
+        banner.style.background = "white";
+        banner.style.textAlign = "center";
+        banner.style.top = 0;
+        banner.style.height = "100%";
+        banner.style.position = "absolute";
+        banner.style.width = "100%";
+        banner.style.zIndex = 1001;
+
+        var bannerText = document.createElement("p");
+        bannerText.appendChild(document.createTextNode("What nickname do you want?"));
+        banner.appendChild(bannerText);
+
+        var form2 = document.createElement("form");
+        var input2 = document.createElement("input");
+
+        form2.appendChild(input2);
+
+        form2.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            name = input2.value;
+            $("#" + id + "banner").parentNode.removeChild($("#" + id + "banner"));
+            input2.focus();
+        });
+
+        banner.appendChild(form2);
+
+        $node.appendChild(banner);
+
+        var messages = document.createElement("div");
+        messages.id = id + "messages";
+        messages.style.whiteSpace = "pre-wrap";
+        messages.style.fontFamily = "sans-serif";
+
+        $node.appendChild(messages);
+
+        // chat input
+        var form = document.createElement("form");
+        form.style.flexShrink = 0;
+
+        var input = document.createElement("input");
+        input.id = id + "input";
+        input.style.width = "100%";
+
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            ws.send("<" + name + "> " + input.value);
+            input.value = "";
+        });
+
+        form.appendChild(input);
+
+        $node.appendChild(form);
+
+        // prevent clicking on inputs from minimizing/maximizing
+        $all("input", function(el) {
+            el.addEventListener("click", function(event) {
+                event.stopPropagation();
             });
-        function toMini(node) {
-            $(node)
-                .addClass("mini")
-                .css({
-                    width: "100px",
-                    height: "100px",
-                    "font-size": '6pt',
-                })
-                .find('> :not(#'+id+'messages)')
-                    .hide();
-            $('#'+id+'messages').css('overflow', 'hidden');
+        }, $node);
+
+        document.body.appendChild($node);
+
+        function toMini() {
+            $node.className = "mini";
+            $node.style.width = "100px";
+            $node.style.height = "100px";
+            $node.style.fontSize = "6pt";
+            $all("#" + id + " > :not(#" + id + "messages)", function(el) {
+                el.style.display = "none";
+            });
+            messages.style.overflow = "hidden";
         }
-        function toMaxi(node) {
-            $(node)
-                .removeClass("mini")
-                .css({
-                    width: "300px",
-                    height: "500px",
-                    "font-size": '12pt',
-                })
-                .find('> :not(#'+id+'messages)')
-                    .show();
-            scrollToBottom($('#'+id+'messages').css({
-                "overflow-y": "auto",
-                "overflow-x": "hidden",
-            })[0]);
+
+        function toMaxi() {
+            $node.className = "";
+            $node.style.width = "300px";
+            $node.style.height = "500px";
+            $node.style.fontSize = "12pt";
+
+            $all("#" + id + " > :not(#" + id + "messages)", function(el) {
+                el.style.display = "block";
+            });
+            messages.style.overflowY = "auto";
+            messages.style.overflowX = "hidden";
+
+            scrollToBottom(messages);
         }
-        var $node = $('<div id=' + id + '>')
-            .append($("<div id="+id+"banner><p>What nickname do you want?<p></div>")
-                .css({
-                    "background": "white",
-                    "text-align": "center",
-                    "top": 0,
-                    "height": "100%",
-                    "position": "absolute",
-                    "width": "100%",
-                    "z-index": 1001,
-                }).append($('<form><input>').submit(function (e) {
-                    e.preventDefault();
-                    name = $(this).find('input')[0].value;
-                    $('#'+id+'banner').remove();
-                    $('#'+id).find('input')[0].focus();
-                })))
-            .append($("<div id="+id+"messages>").css({
-                    'white-space': 'pre-wrap',
-                    'font-family': 'sans-serif',
-                }))
-            .append($input)
-            .css({
-                position: "absolute",
-                right: "50px",
-                top: "50px",
-                border: "solid thin black",
-                "background-color": "white",
-                "z-index": 1000,
-                display: "flex",
-                "flex-direction": "column",
-                "justify-content": "flex-end",
-            }).on("click", "input", function () {
-                return false;
-            }).click(function(e) {
-                e.preventDefault();
-                if ($(this).hasClass("mini")) {
-                    toMaxi(this);
-                } else {
-                    toMini(this);
-                }
-            }).appendTo('body');
-        toMini($node[0]);
+
+        toMini($node);
+
         var ws = new WebSocket(getWebsocketUrl());
-        ws.onopen = function onopen() {
+        ws.onopen = function() {
         };
-        ws.onmessage = function onmessage(evt) {
-            var el = document.getElementById(id+'messages');
-            var atbottom = isScrolledToBottom(el);
-            $('<div>').text(evt.data).appendTo(el);
-            if (atbottom || $('#'+id).hasClass("mini")) {
+        ws.onmessage = function(event) {
+            var el = $("#" + id + "messages");
+
+            var message = document.createElement("div");
+            message.appendChild(document.createTextNode(event.data));
+            el.appendChild(message);
+
+            if (isScrolledToBottom(el) || $("#" + id).className === "mini") {
                 scrollToBottom(el);
             }
         };
@@ -138,25 +190,14 @@
     // wait time is cumulative). All numbers were chosen because they felt
     // right. The reason for doing this is allowing this script to be included
     // before the jquery library and still work.
-    function chatroomwidget_loader(retry_ms) {
-        if (typeof jQuery === 'undefined') {
-            // try again a couple times
-            if (retry_ms > 1000) {
-                throw "chatroomwidget.js requires jQuery";
-            } else {
-                setTimeout(function () {
-                    chatroomwidget_loader(retry_ms * 1.4);
-                }, retry_ms);
-            }
-        } else {
-            chatroomwidget_main(jQuery);
-        }
+    function chatroomwidget_loader() {
+        chatroomwidget_main();
     }
 
     if (typeof define === "function" && define.amd) {
-        define("chatroomwidget", ["jquery"], chatroomwidget_main);
+        define("chatroomwidget", [], chatroomwidget_main);
     } else {
-        chatroomwidget_loader(20);
+        chatroomwidget_loader();
     }
 
 })();
